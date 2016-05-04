4e806f051a1a50ef627b6fe42a48876c4c48ccf9
Karen J Hernandez <karenjhernandez@gmail.com>
2016-05-04 01:38:11 -0400

    added loops and func folder

---
+++ loopsfunctions/loopsfnc.js
@@ -0,0 +1,15 @@
+var mets =['Noah', 'Matt', 'Steven', 'Jacob'];
+
+function Greet(mets){
+	console.log ('Hello ' + mets)
+}
+Greet(mets)
+
+
+var mets = ['Hello ' + 'Noah', 'Hello ' +'Matt', 'Hello ' +'Steven', 'Hello ' +'Jacob'];
+var Greet = mets.length;
+for (var i = 0; i < Greet; i++) {
+    console.log(mets[i]);
+   
+}
+
