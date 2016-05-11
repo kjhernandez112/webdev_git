50cc0653125f613ab48dfe88ef765c4cbcfe9dbe
Karen J Hernandez <karenjhernandez@gmail.com>
2016-05-11 00:36:09 -0400

    added jquery, hide, delay, fadeIn, animate

---
+++ jQuery/jQuery.js
@@ -0,0 +1,12 @@
+
+
+$('body').css('background-color', '#eceaca');	
+$('ul').css('background-color','#ff004d');
+$('#six').text();
+$('#four').animate().delay(1000);
+
+
+$('ul').html()
+$('li').hide().delay(500).fadeIn(1400);
+
+
